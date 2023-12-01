390630ccc96771c2352a93edc7d57085ccea877e
whl <ewanghailong@vip.qq.com>
2023-12-01 16:57:42 +0800

    添加基本布局

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,163 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Greet from "./components/Greet.vue";
+const serialNameData = [
+  {
+    key: "node1",
+    title: "Leaf",
+  },
+];
+const baudRateData = [
+  {
+    key: "node1",
+    title: "9600",
+  },
+  {
+    key: "node2",
+    title: "38400",
+  },
+  {
+    key: "node3",
+    title: "115200",
+  },
+];
+const dataBitsData = [
+  {
+    key: "node1",
+    title: "7",
+  },
+  {
+    key: "node2",
+    title: "8",
+  },
+];
+const parityData = [
+  {
+    key: "node1",
+    title: "None",
+  },
+  {
+    key: "node2",
+    title: "Odd",
+  },
+  {
+    key: "node3",
+    title: "Even",
+  },
+  {
+    key: "node4",
+    title: "Mark",
+  },
+  {
+    key: "node5",
+    title: "Space",
+  },
+];
+const stopBitsData = [
+  {
+    key: "node1",
+    title: "1",
+  },
+  {
+    key: "node2",
+    title: "1.5",
+  },
+  {
+    key: "node3",
+    title: "2",
+  },
+];
 </script>
 
 <template>
-  <div class="container">
-    <a-layout>
-      <div>
-        串口号
-        <a-dropdown @select="handleSelect">
-          <a-button>Click Me</a-button>
-          <template #content>
-            <a-doption>Option 1</a-doption>
-            <a-doption disabled>Option 2</a-doption>
-            <a-doption :value="{ value: 'Option3' }">Option 3</a-doption>
-          </template>
-        </a-dropdown>
+  <a-grid :cols="2" :colGap="0" :rowGap="16">
+    <a-grid-item>
+      <div class="container">
+        <a-grid :cols="2" :colGap="0" :rowGap="16">
+          <a-grid-item>串口号</a-grid-item>
+          <a-grid-item>
+            <a-tree-select
+              :data="baudRateData"
+              :label-in-value="true"
+              :default-value="{ value: 'node1', label: '9600' }"
+              placeholder="Please select ..."
+              style="width: 300px"
+              @change="onChange"
+            ></a-tree-select>
+          </a-grid-item>
+          <a-grid-item>波特率</a-grid-item>
+          <a-grid-item>
+            <a-tree-select
+              :data="baudRateData"
+              :label-in-value="true"
+              :default-value="{ value: 'node1', label: '9600' }"
+              placeholder="Please select ..."
+              style="width: 300px"
+              @change="onChange"
+            ></a-tree-select
+          ></a-grid-item>
+          <a-grid-item>数据位</a-grid-item>
+          <a-grid-item>
+            <a-tree-select
+              :data="dataBitsData"
+              :label-in-value="true"
+              :default-value="{ value: 'node2', label: '8' }"
+              placeholder="Please select ..."
+              style="width: 300px"
+              @change="onChange"
+            ></a-tree-select
+          ></a-grid-item>
+          <a-grid-item>校验位</a-grid-item>
+          <a-grid-item>
+            <a-tree-select
+              :data="parityData"
+              :label-in-value="true"
+              :default-value="{ value: 'node1', label: 'None' }"
+              placeholder="Please select ..."
+              style="width: 300px"
+              @change="onChange"
+            ></a-tree-select
+          ></a-grid-item>
+          <a-grid-item>停止位</a-grid-item>
+          <a-grid-item>
+            <a-tree-select
+              :data="stopBitsData"
+              :label-in-value="true"
+              :default-value="{ value: 'node1', label: '1' }"
+              placeholder="Please select ..."
+              style="width: 300px"
+              @change="onChange"
+            ></a-tree-select
+          ></a-grid-item>
+          <a-grid-item :span="2">
+            <a-button type="outline" long>连接</a-button>
+          </a-grid-item>
+        </a-grid>
       </div>
+    </a-grid-item>
+    <a-grid-item>
       <div>
-        波特率
-        <a-dropdown @select="handleSelect">
-          <a-button>Click Me</a-button>
-          <template #content>
-            <a-doption>Option 1</a-doption>
-            <a-doption disabled>Option 2</a-doption>
-            <a-doption :value="{ value: 'Option3' }">Option 3</a-doption>
-          </template>
-        </a-dropdown>
+        <a-textarea placeholder="Please enter something" allow-clear /></div
+    ></a-grid-item>
+    <a-grid-item>
+      <div class="container">
+        <a-grid :cols="2" :colGap="0" :rowGap="16">
+          <a-grid-item>接收字节:</a-grid-item>
+          <a-grid-item>0B</a-grid-item>
+          <a-grid-item>发送字节:</a-grid-item>
+          <a-grid-item>0B</a-grid-item>
+          <a-grid-item>接收速度:</a-grid-item>
+          <a-grid-item>0.00B/S</a-grid-item>
+          <a-grid-item>发送速度:</a-grid-item>
+          <a-grid-item>0.00B/S</a-grid-item>
+        </a-grid>
       </div>
-    </a-layout>
-  </div>
+    </a-grid-item>
+    <a-grid-item>
+      <div>
+        <a-textarea
+          placeholder="Please enter something"
+          allow-clear
+        /></div></a-grid-item
+  ></a-grid>
 </template>
 
 <style scoped>
